46c1c3260a22505ea6a157d691dace31417c1c28
ZodiakoU <cristianjvz98@gmail.com>
2023-09-01 08:10:22 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ernestoverdugo.guia2',
+  appName: 'iguia2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
