d7034d98437fb06e8472e713e6fd1a9443a1e63c
ZodiakoU <cristianjvz98@gmail.com>
2023-09-01 09:12:55 -0500

    agregados lista generos y tabgenero

--- src/app/lista-peliculas.ts
+++ src/app/lista-peliculas.ts
@@ -1,5 +1,24 @@
 import { Pelicula } from "./pelicula";
 
 export const PELICULAS : Pelicula[] = [
-    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"}
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
+    {id: 1, nombre: "matrix", genero: "string", director: "string",  portada: "string"},
 ]
