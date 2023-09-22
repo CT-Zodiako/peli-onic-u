445432606b82787e26ea84f5df782fe98446015d
Zodiako <cristianjvz98@gmail.com>
2023-09-22 01:07:13 -0500

    feat: guia 4 finalizada

--- src/app/lista-actores.ts
+++ src/app/lista-actores.ts
@@ -1,5 +1,7 @@
 import { Actor } from "./actor";
 
 export const ACTORES : Actor[] = [
-    {id: 1, nombre_actor: 'el chapulin colorado', id_pelicula: 1}
+    { id: 1, nombre_actor: 'El Chapulín Colorado', id_pelicula: 1 },
+    { id: 2, nombre_actor: 'Mario Bros', id_pelicula: 2 },
+    { id: 3, nombre_actor: 'Indiana Jones', id_pelicula: 3 }
 ]
--- src/app/lista-peliculas.ts
+++ src/app/lista-peliculas.ts
@@ -3,142 +3,43 @@ import { Pelicula } from './pelicula';
 export const PELICULAS: Pelicula[] = [
   {
     id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
+    nombre: 'Matrix',
+    genero: 'Ciencia ficción',
+    director: 'Lana Wachowski, Lilly Wachowski',
+    portada: 'https://example.com/portada_matrix.jpg'
   },
   {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
+    id: 2,
+    nombre: 'Pulp Fiction',
+    genero: 'Crimen',
+    director: 'Quentin Tarantino',
+    portada: 'https://example.com/portada_pulp_fiction.jpg'
   },
   {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
+    id: 3,
+    nombre: 'Inception',
+    genero: 'Ciencia ficción',
+    director: 'Christopher Nolan',
+    portada: 'https://example.com/portada_inception.jpg'
   },
   {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
+    id: 4,
+    nombre: 'The Shawshank Redemption',
+    genero: 'Drama',
+    director: 'Frank Darabont',
+    portada: 'https://example.com/portada_shawshank_redemption.jpg'
   },
   {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
-  {
-    id: 1,
-    nombre: 'matrix',
-    genero: 'string',
-    director: 'string',
-    portada: 'string',
-  },
+  id: 5,
+  nombre: 'The Dark Knight',
+  genero: 'Acción',
+  director: 'Christopher Nolan',
+  portada: 'https://example.com/portada_dark_knight.jpg'
+}
+
+  
+  
+  
+  
+ 
 ];
